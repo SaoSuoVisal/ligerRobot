ab512fdf0164206ad5a2e7923f806d21cad98cb9
visalsao <visal.s@ligercambodia.org>
2016-11-08 15:20:34 +0700

    Add some new files in...

--- Autonomus Peroid/Potentiolmeter.c
+++ Autonomus Peroid/Potentiolmeter.c
@@ -20,11 +20,12 @@ void resetArm(){
 	}
 }
 
+
 void resetGrab(){
 	if(SensorValue(potentiometerGrab) <= 3000){//10
 		motor[grabMotor]= 100; // move the motor at a speed of 100
 		waitUntil(SensorValue(potentiometerGrab) > 3000); // if the potentiometer is bigger than 2000
-		stopMotor(grabMotor);// Stop the motor
+		stopMotor(grabMotor); // Stop the motor
 	}else{
 		stopMotor(grabMotor);
 	}
@@ -70,54 +71,99 @@ void turnRight(int speed){
 task main()
 {
 
-////////////////////////////////////////////////////////
-//                 First Motion	  										//
-////////////////////////////////////////////////////////
-resetArm();
-resetGrab();
+	////////////////////////////////////////////////////////
+	//                 First Motion	  										//
+	////////////////////////////////////////////////////////
+	resetArm();
+	resetGrab();
 
-//Move Stright to the cube
-move(127); // at the maximume speed
-wait1Msec(3000); // for 2 seconds
+	//Move Stright to the cube
+	move(127); // at the maximume speed
+	wait1Msec(3000); // for 2 seconds
 
-grabing(127); // Starts to grab
-move(127); // go stright
-wait1Msec(3000); // for 2 seconds
+	grabing(127); // Starts to grab
+	move(127); // go stright
+	wait1Msec(3000); // for 2 seconds
 
-lifting(127); // Move the arm up
-droping(127); // release it
+	lifting(127); // Move the arm up
+	droping(127); // release it
 
-turnLeft(100); // turn left
-wait1Msec(500); // for 300 micosecond
-move(127); // Move forward at a maximume speed
-wait1Msec(5000); // for 5 second
+	turnLeft(100); // turn left
+	wait1Msec(500); // for 300 micosecond
+	move(127); // Move forward at a maximume speed
+	wait1Msec(5000); // for 5 second
 
-turnLeft(100); // Turn left back at a speed of 100
-move(127); // Turn left back at a miximume speed
-wait1Msec(500); // for 500 microsecond
+	turnLeft(100); // Turn left back at a speed of 100
+	move(127); // Turn left back at a miximume speed
+	wait1Msec(500); // for 500 microsecond
 
 
 
-////////////////////////////////////////////////////////
-//                 Second Motion											//
-////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////
+	//                 Second Motion											//
+	////////////////////////////////////////////////////////
 
-move(127); //move straight to stars
-wait1Msec(3000); // keep moving forward for three 3 seconds
+	// Use this code, if our robot started at the right of the competition field
 
-move(127);	//keeping moving
-grabing(127); //while moving grabing
-wait1Msec(2000) // keep the action for 2 seconds
+	move(127); //move straight to stars
+	wait1Msec(3000); // keep moving forward for three 3 seconds
 
-turnRight(100);// trun right
-wait1Msec(2000); //move for 2 seconds
+	move(127);	//keeping moving
+	grabing(127); //while moving grabing
+	wait1Msec(2000); // keep the action for 2 seconds
 
-move(127); // move forward
-wait1Msec(3000) //going for 3 seconds
+	turnRight(100);//turn right (replace here for the exavt measurement, once figure out the exact measurement)
+	wait1Msec(2000); //move for 2 seconds
 
-lifting(100); // lift the arm up
-wait1Msec(2000) //getting the arm up for 2 seconds
+	move(127); // move forward
+	wait1Msec(3000) //going for 3 seconds
 
-droping(127); // droping the object
+	lifting(100); // lift the arm up
+	wait1Msec(2000) //getting the arm up for 2 seconds
 
+	droping(127); // droping the object
+	wait1Msec (1000); //drop with in one second
+
+	turnLeft (100); // then turn left (should replace with the exact measuremen, once figure out the exact measurement)
+	wait1Msec (2000);
+
+	move (127);//move forward
+	wait1Msec (2000) //move for two seconds
+
+	move (-127) //move backward
+	wait1Msec (3000) //for3 seconds
+
+	////////////////////////////////////////////////////////
+	//                 Third Motion											  //
+	////////////////////////////////////////////////////////
+
+	// Use this code, when our robot started of at the left of the competition board
+
+	move(127); //move straight to stars
+	wait1Msec(3000); // keep moving forward for three 3 seconds
+
+	move(127);	//keeping moving
+	grabing(127); //while moving grabing
+	wait1Msec(2000); // keep the action for 2 seconds
+
+	turnLeft(100);//turn left (replace here for the exavt measurement, once figure out the exact measurement)
+	wait1Msec(2000); //move for 2 seconds
+
+	move(127); // move forward
+	wait1Msec(3000) //going for 3 seconds
+
+	lifting(100); // lift the arm up
+	wait1Msec(2000) //getting the arm up for 2 seconds
+
+	droping(127); // droping the object
+	wait1Msec (1000); //drop with in one second
+
+	turnRight (100); // then turn right (should replace with the exact measuremen, once figure out the exact measurement)
+	wait1Msec (2000);
+
+	move (127);//move forward
+	wait1Msec (2000) //move for two seconds
+
+	move (-127) //move backward
+	wait1Msec (3000) //for3 seconds
 }
