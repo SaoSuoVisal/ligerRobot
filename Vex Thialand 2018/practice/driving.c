b998cc242cc1ec0e5009ed924b058331b3dafb2d
Unknown <ketya.n@ligercambodia.org>
2018-01-23 16:11:33 +0700

    Partner Joystick
    enable scissors control for partner joystick

--- Vex Thialand 2018/practice/driving.c
+++ Vex Thialand 2018/practice/driving.c
@@ -14,31 +14,31 @@ int limitRPM = 160; // For high torque motor the has a limit speed
 int sisSpeed = 127; // For scissors speed
 /*
 void sciEqual(){
-	int maxSpeed = 117;
-	int potThreshold = 30;
-	int maxDiff = 150;
+int maxSpeed = 117;
+int potThreshold = 30;
+int maxDiff = 150;
 
-	int err = abs(SensorValue[scissorsRight] - SensorValue[scissorsLeft]);
-	if(){
-	}
+int err = abs(SensorValue[scissorsRight] - SensorValue[scissorsLeft]);
+if(){
+}
 }*/
 
 task main()
 {
 	while(true){
+		//get input into axis
 		int y = vexRT[Ch2];
 		int x = vexRT[Ch1];
 		int r = vexRT[Ch4];
 
+		if(abs(y) > threshold && abs(y) < limitRPM)	y = vexRT[Ch2];
+		else	y = 0;
 
-   if(abs(y) > threshold && abs(y) < limitRPM)	y = vexRT[Ch2];
-   else	y = 0;
+		if(abs(x) > threshold && abs(x) < limitRPM)	x = vexRT[Ch1];
+		else	x = 0;
 
-   if(abs(x) > threshold && abs(x) < limitRPM)	x = vexRT[Ch1];
-   else	x = 0;
-
-   if(abs(r) > threshold)	r = vexRT[Ch4]/2;
-   else	r = 0;
+		if(abs(r) > threshold)	r = vexRT[Ch4]/2;
+		else	r = 0;
 
 		// Y component, X component, Rotation
 		motor[frontLeft] =   y + x + r;
@@ -46,28 +46,28 @@ task main()
 		motor[backRight] =  -y - x + r;
 		motor[backLeft] =   y - x + r;
 
-		// Arm Control
-    if(vexRT[Btn6U] == 1){
-      motor[mobileGoalLift] = 90;
-    } else if(vexRT[Btn6D] == 1){
-      motor[mobileGoalLift] = -90;
-    } else{
-      motor[mobileGoalLift] = 0;
-    }
+		// MobileGoal Lift Control
+		if(vexRT[Btn6U] == 1){
+			motor[mobileGoalLift] = 90;
+			} else if(vexRT[Btn6D] == 1){
+			motor[mobileGoalLift] = -90;
+			} else{
+			motor[mobileGoalLift] = 0;
+		}
 
-    // Scissors Lift
-    if(vexRT[Btn5U] == 1){
-      motor[scissorsLeft] = sisSpeed;
-      motor[scissorsRight] = sisSpeed;
-    } else if(vexRT[Btn5D] == 1){
-      motor[scissorsLeft] = -sisSpeed;
-      motor[scissorsRight] = -sisSpeed;
-    } else{
-      motor[scissorsLeft] = 0;
-      motor[scissorsRight] = 0;
-    }
+		// Scissors Lift
+		if(vexRT[Btn5U] == 1 || vexRT[Btn5UXmtr2] == 1){
+			motor[scissorsLeft] = sisSpeed;
+			motor[scissorsRight] = sisSpeed;
+			} else if(vexRT[Btn5D] == 1 || vexRT[Btn5DXmtr2] == 1){
+			motor[scissorsLeft] = -sisSpeed;
+			motor[scissorsRight] = -sisSpeed;
+			} else{
+			motor[scissorsLeft] = 0;
+			motor[scissorsRight] = 0;
+		}
 
-    // Motor values can only be updated every 20ms
+		// Motor values can only be updated every 20ms
 		wait10Msec(2);
 	}
 }
